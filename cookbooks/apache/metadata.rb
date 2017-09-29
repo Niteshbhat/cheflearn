bdc7df5c46b569d43c9a7d7fe136683a4152c197
root <root@nitbha2.mylabserver.com>
2017-09-29 10:01:12 +0000

    new recipes

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,6 +1,6 @@
 name 'apache'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Nitesh bhat'
+maintainer_email 'bhatnitesh@powershell.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'apache' do 
+     package_name 'httpd'
+         action:install
+         end 
+
+         service 'apache' do
+               service_name 'httpd'
+                 action[:start,:enable]
+
+                end
